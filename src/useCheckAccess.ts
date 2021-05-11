2ece79566a2f6a5b32843a76594349c41883afa5
Ivaylo Ushev <iushev@yahoo.com>
2021-05-11 12:34:43 +0300

    Update useCheckAccess

--- src/useCheckAccess.ts
+++ src/useCheckAccess.ts
@@ -1,43 +1,52 @@
 import { useContext, useEffect, useRef, useState } from "react";
 import RbacContext, { CheckAccessOptions } from "./RbacContext";
 
+export type CheckAssessState = {
+  checking: boolean;
+  hasAccess: boolean;
+};
+
 function useCheckAccess({ roles, allow, params, match }: CheckAccessOptions) {
-  const componentIsMounted = useRef(true);
   const rbac = useContext(RbacContext);
 
-  const [checking, setChecking] = useState(false);
-  const [hasAccess, setHasAccess] = useState(false);
+  const [state, setState] = useState<CheckAssessState>({
+    checking: true,
+    hasAccess: false,
+  });
 
   useEffect(() => {
-    return () => {
-      componentIsMounted.current = false;
-    };
-  }, []);
-
-  useEffect(() => {
-    setChecking(true);
-    setHasAccess(false);
+    let subscribed = true;
+    setState({
+      checking: true,
+      hasAccess: false,
+    });
 
     rbac
       .checkAccess({ roles, allow, params, match })
       .then((result) => {
-        if (componentIsMounted.current) {
-          setChecking(false);
-          setHasAccess(result);
+        if (subscribed) {
+          setState({
+            checking: false,
+            hasAccess: result,
+          });
         }
       })
       .catch((err) => {
-        if (componentIsMounted.current) {
-          setChecking(false);
-          setHasAccess(false);
+        console.error(err);
+        if (subscribed) {
+          setState({
+            checking: false,
+            hasAccess: false,
+          });
         }
       });
+
+    return () => {
+      subscribed = false;
+    };
   }, [rbac, roles, allow, params, match]);
 
-  return {
-    checking,
-    hasAccess,
-  };
+  return state;
 }
 
 export default useCheckAccess;
