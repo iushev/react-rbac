7dc738e542cee57ac70b75ce211884fae336ec9f
Ivaylo Ushev <iushev@yahoo.com>
2022-05-20 11:47:21 +0300

    exposed useRbac

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { default as RbacContext, RbacConsumer, RbacProvider } from "./RbacContext";
+export { default as RbacContext, RbacConsumer, RbacProvider, useRbac } from "./RbacContext";
 export { default as CheckAccess, CheckAccessProps } from "./CheckAccess";
 export { default as NoAccess } from "./NoAccess";
 export { default as useCheckAccess } from "./useCheckAccess";
